9a3cb655b4dcd1ae14465ca7dc4950a64553327c
Ma7en <m9ee9m@gmail.com>
2024-07-12 00:04:38 +0300

    edit main menu

--- FileProject.sh
+++ FileProject.sh
@@ -1,5 +1,7 @@
- #When running the file, check the database folder exists or not.
- #!/bin/bash
+#! /usr/bin/bash
+shopt -s extglob
+
+#When running the file, check the database folder exists or not.
 if [ ! -d "./DB" ]
 then
     echo "Database directory does not exist. Creating..."
@@ -9,54 +11,106 @@ cd DB
 echo "Access DB folder"
 
 
-
-
-
-
-
-# main menu 
+# Main menu 
 main_menu() {
-    echo "Select an option:- "
-    echo "1- Create Database"
-    echo "2- List Databases"
-    echo "3- Connect To Database"
-    echo "4- Drop Database"
-    echo "5- Exit"
-    read -p "Enter your selection number:- " choice
+    while true
+    do
+        
+        echo "Select an option:- "
+        echo "1- Create Database"
+        echo "2- List Databases"
+        echo "3- Connect To Database"
+        echo "4- Drop Database"
+        echo "5- Exit"
 
-    case $choice in
-        1) 
-            create_database 
-            ;;
+        read -p "Enter your selection number:- " choice
 
-        2) 
-            list_databases 
-            ;;
+        case $choice in
+            1) 
+                create_database 
+                ;;
 
-        3) 
-            connect_to_database 
-            ;;
+            2) 
+                list_databases 
+                ;;
 
-        4) 
-            drop_database 
-            ;;
+            3) 
+                connect_to_database 
+                ;;
 
-        5) 
-            # exit 
-            break
-            ;;
-    
-        *) 
-            echo "Invalid choice"
-            ;;
-    esac
+            4) 
+                drop_database 
+                ;;
+
+            5) 
+                # exit 
+                break
+                ;;
+        
+            *) 
+                echo "Invalid choice"
+                ;;
+        esac
+    done
 
 }
+
+# 1 = create a new database
+create_database() {
+    read -p "Enter database name:- " db_name
+    if [[ "$db_name" =~ [[:punct:][:space:]] ]]
+    then
+        echo "Invalid name. No spaces or special characters allowed."
+        return
+    fi
+
+    if [[ -z "$db_name" ]]
+    then
+        echo "Invalid name. Please enter a valid name."
+        return
+    fi
+
+    if [ -d "$db_name" ]
+    then
+        echo "Database already exists."
+    else
+        mkdir "$db_name"
+        echo "Database ($db_name) created."
+    fi
+}
+
+# 2 = list DB 
+list_databases() {
+    ls -d */
+}
+
+# 4 = drop database
+drop_database() {
+    read -p "Enter database name: " db_name
+    if [[ "$db_name" =~ [[:punct:][:space:]] ]]
+    then
+        echo "Invalid name. No spaces or special characters allowed."
+        return
+    fi
+
+    if [[ -z "$db_name" ]]
+    then
+        echo "Invalid name. Please enter a valid name."
+        return
+    fi
+
+    if [ -d "$db_name" ]
+    then
+        rm -r "$db_name"
+        echo "Database ($db_name) deleted."
+    else
+        echo "Database does not exist."
+    fi
+}
+
+# 3 = Contact DB 
 
 
 
 # Start the application
-while true
-do
-    main_menu
-done
+main_menu
